91281c10ac1f3b04f6556cae61fa5d5051d55df0
Tranku <148291727+Tranku28@users.noreply.github.com>
2025-10-11 13:17:17 +0200

    Move & camera rotation

--- Assets/Scripts/PlayerMovementController.cs
+++ Assets/Scripts/PlayerMovementController.cs
@@ -1,16 +1,53 @@
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovementController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private float movementSensivity;
+    
+    private Camera _playerCamera;
+    private InputAction _lookInput;
+    private InputAction _moveInput;
+    private Rigidbody _playerRb;
+    
+    private float _yMoveOffset, _xMoveOffset;
+
+    private void Awake()
+    {
+        _playerCamera = GetComponentInChildren<Camera>();
+        _playerRb = GetComponent<Rigidbody>();
+        
+        _lookInput = InputSystem.actions.FindAction("Look");
+        _moveInput = InputSystem.actions.FindAction("Move");
+    }
+    
     void Start()
     {
         
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
+        Rotate();
+    }
+
+    private void FixedUpdate()
+    {
+        var input = _moveInput.ReadValue<Vector2>();
+        var move = new Vector3(input.x * movementSensivity, 0f, input.y * movementSensivity);
+        var moveDirection = transform.TransformDirection(move);
         
+        _playerRb.linearVelocity = moveDirection;
+    }
+
+    private void Rotate()
+    {
+        _yMoveOffset += _lookInput.ReadValue<Vector2>().y * movementSensivity * Time.deltaTime;
+        _xMoveOffset = _lookInput.ReadValue<Vector2>().x * movementSensivity * Time.deltaTime;
+        
+        var clampedAngle = Mathf.Clamp(-_yMoveOffset, -55f, 55f);
+        _playerCamera.transform.localRotation = Quaternion.Euler(clampedAngle, 0f, 0f);
+        transform.Rotate(0, _xMoveOffset, 0);
     }
 }
