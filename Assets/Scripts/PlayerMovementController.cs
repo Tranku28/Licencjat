e924d73789425997daf896ee3d359d9dd5b6f27e
Tranku <148291727+Tranku28@users.noreply.github.com>
2025-10-15 19:38:54 +0200

    alt movement

--- Assets/Scripts/PlayerMovementController.cs
+++ Assets/Scripts/PlayerMovementController.cs
@@ -4,47 +4,39 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovementController : MonoBehaviour
 {
-    [SerializeField] private float movementSensivity;
+    [SerializeField] private float mouseSensitivity;
+    [SerializeField] private float movementSpeed;
     
     private Camera _playerCamera;
     private InputAction _lookInput;
     private InputAction _moveInput;
-    private Rigidbody _playerRb;
+    private CharacterController _characterController;
     
     private float _yMoveOffset, _xMoveOffset;
 
     private void Awake()
     {
         _playerCamera = GetComponentInChildren<Camera>();
-        _playerRb = GetComponent<Rigidbody>();
+        _characterController = GetComponent<CharacterController>();
         
         _lookInput = InputSystem.actions.FindAction("Look");
         _moveInput = InputSystem.actions.FindAction("Move");
     }
     
-    void Start()
-    {
-        
-    }
-    
     void Update()
     {
         Rotate();
-    }
-
-    private void FixedUpdate()
-    {
-        var input = _moveInput.ReadValue<Vector2>();
-        var move = new Vector3(input.x * movementSensivity, 0f, input.y * movementSensivity);
-        var moveDirection = transform.TransformDirection(move);
         
-        _playerRb.linearVelocity = moveDirection;
+        var input = _moveInput.ReadValue<Vector2>();
+        var moveDirection = transform.forward * (input.y * movementSpeed);
+        
+        _characterController.SimpleMove(moveDirection + new Vector3(input.x, 0, input.y));
     }
 
     private void Rotate()
     {
-        _yMoveOffset += _lookInput.ReadValue<Vector2>().y * movementSensivity * Time.deltaTime;
-        _xMoveOffset = _lookInput.ReadValue<Vector2>().x * movementSensivity * Time.deltaTime;
+        _yMoveOffset += _lookInput.ReadValue<Vector2>().y * mouseSensitivity * Time.deltaTime;
+        _xMoveOffset = _lookInput.ReadValue<Vector2>().x * mouseSensitivity * Time.deltaTime;
         
         var clampedAngle = Mathf.Clamp(-_yMoveOffset, -55f, 55f);
         _playerCamera.transform.localRotation = Quaternion.Euler(clampedAngle, 0f, 0f);
