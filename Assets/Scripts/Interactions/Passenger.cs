fd23800992961d2621ee3944b7b715f68684b735
Tranku <148291727+Tranku28@users.noreply.github.com>
2025-10-30 14:01:24 +0100

    Passenger Interaction & Ticket Update

---
+++ Assets/Scripts/Interactions/Passenger.cs
@@ -0,0 +1,19 @@
+using System;
+using Scriptable_Objects;
+using UnityEngine;
+
+namespace Interactions
+{
+    public class Passenger : MonoBehaviour, IInteractable
+    {
+        [SerializeField] private PassengerData passengerData;
+        
+        public static Action<PassengerData> OnPassengerInteracted;
+
+        public void Interact()
+        {
+            Debug.Log("Interacted");
+            OnPassengerInteracted?.Invoke(passengerData);
+        }
+    }
+}
