f204de94ccdbd6a62a9c1d4d24195a5618cf4577
Tranku <148291727+Tranku28@users.noreply.github.com>
2026-05-24 20:41:12 +0200

    New Sounds 2 and dragon logic

--- Assets/Scripts/GameMechanics/Interactions/Souvenir.cs
+++ Assets/Scripts/GameMechanics/Interactions/Souvenir.cs
@@ -1,9 +1,14 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using Core;
 using Core.Save_System;
 using Core.Scriptable_Objects;
 using Core.Scriptable_Objects.Souvenirs;
+using DG.Tweening;
+using EasyTextEffects.Editor.MyBoxCopy.Extensions;
+using FMOD.Studio;
+using FMODUnity;
 using GameMechanics.Interactions;
 using SouvenirSystem;
 using UnityEngine;
@@ -11,11 +16,16 @@ using UnityEngine.UI;
 
 public class Souvenir : MonoBehaviour, IInteractable
 {
+    private static WaitForSeconds _waitForSeconds1 = new(1f);
     [SerializeField] private SouvenirData souvenirData;
+    [SerializeField] private bool opensSouvenirUI = true;
+    [field: SerializeField] private EventReference souvenirInteractionSound {get; set;}
+    private EventInstance _souvenirEventInstance;
     private SouvenirEffect _effect;
     private SouvenirEffectResolver _effectResolver;
     public SouvenirEffect Effect => _effect; 
     private Button _useButton;
+    private bool _soundPlays;
     public int ID { get; private set; }
 
     public event Action<Souvenir> OnSouvenirUsed;
@@ -42,7 +52,26 @@ public class Souvenir : MonoBehaviour, IInteractable
 
     public void Interact()
     {
+        if (!opensSouvenirUI)
+        {
+            if (!souvenirInteractionSound.IsNull && !_soundPlays)
+            {
+                _soundPlays = true;
+                StartCoroutine(WaitTillSoundPlayAgain());
+            }
+
+            return;
+        }
+
         OnSouvenirInteracted?.Invoke(souvenirData);
+    }
+
+    private IEnumerator WaitTillSoundPlayAgain()
+    {
+        AudioManager.Instance.PlayOneShot(souvenirInteractionSound, transform.position);
+        yield return _waitForSeconds1;
+
+        _soundPlays = false;
     }
 
     public void ApplyEffects()
